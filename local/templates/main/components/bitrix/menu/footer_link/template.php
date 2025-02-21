0870ab7949ad695239a369d2e683208b6ad1f01b
Yuriy Baev <y.baev95@mail.ru>
2025-02-21 16:27:04 +0300

    header

--- local/templates/main/components/bitrix/menu/footer_link/template.php
+++ local/templates/main/components/bitrix/menu/footer_link/template.php
@@ -18,5 +18,91 @@ $this->setFrameMode(true);
 	<? // var_dump($item); 
 	?>
 
+
+
+
+	<nav class="footer__nav footer__separator">
+
+		<div class="footer__nav_item">
+			<p class="footer__nav_acc">Психолог</p>
+			<ul>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+				<li><a href="#">Лечение бессонницы</a></li>
+				<li><a href="#">Психологическая помощь</a></li>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+			</ul>
+		</div>
+		<div class="footer__nav_item">
+			<p class="footer__nav_acc">Врач-психиатр</p>
+			<ul>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+				<li><a href="#">Лечение бессонницы</a></li>
+				<li><a href="#">Психологическая помощь</a></li>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+			</ul>
+		</div>
+		<div class="footer__nav_item">
+			<p class="footer__nav_acc">Лечение зависимостей</p>
+			<ul>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+				<li><a href="#">Лечение бессонницы</a></li>
+				<li><a href="#">Психологическая помощь</a></li>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+			</ul>
+		</div>
+		<div class="footer__nav_item">
+			<p class="footer__nav_acc">Психиатрическая помощь</p>
+			<ul>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+				<li><a href="#">Лечение бессонницы</a></li>
+				<li><a href="#">Психологическая помощь</a></li>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+			</ul>
+		</div>
+		<div class="footer__nav_item">
+			<p class="footer__nav_acc">Психические расстройства</p>
+			<ul>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+				<li><a href="#">Лечение бессонницы</a></li>
+				<li><a href="#">Психологическая помощь</a></li>
+				<li><a href="#">Консультация сексолога</a></li>
+				<li><a href="#">Психолог сексолог</a></li>
+			</ul>
+		</div>
+		<div class="footer__nav_item footer__nav_item-menu">
+			<ul>
+				<li><a href="#">О клинике</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Отзывы</a></li>
+				<li><a href="#">Врачи</a></li>
+				<li><a href="#">Услуги</a></li>
+				<li><a href="#">Контакты</a></li>
+				<li><a href="#">Статьи</a></li>
+				<li><a href="#">Цены</a></li>
+				<li><a href="#">Акции</a></li>
+			</ul>
+		</div>
+
+
+	</nav>
+
+
+
+
+
+
+
+
+
+
 	<li><a href="<?= $item['LINK'] ?>"><?= $item['TEXT'] ?></a></li>
 <? endforeach ?>
--- local/templates/main/components/bitrix/menu/services_link/template.php
+++ local/templates/main/components/bitrix/menu/services_link/template.php
@@ -14,48 +14,32 @@
 $this->setFrameMode(true);
 $arr_parent = $arr_child =  [];
 $i = 0;
+
+
+// var_dump($arResult);
+
 if (CModule::IncludeModule("iblock")) {
 	$arr_html = $arr_tabs = [];
-	foreach ($arResult as $item):
-		$class = $i == 0 ? "tab__btn--active" : "";
+	foreach ($arResult as $item) {
+		$class = $i == 0 ? "active" : "";
 		$arr_tabs[] = <<<HTML
-			<button class="header__submenu_tab-btn tab__btn $class" data-id="{$item['PARAMS']['ID']}">
+			<button class="header__submenu_tab-btn $class" data-id="{$item['PARAMS']['ID']}">
 				{$item['TEXT']}
 			</button>
 		HTML;
-		$class = $i++ == 0 ? "tabcontent--active" : "";
+		$class = $i++ == 0 ? "active" : "";
 		$arr_child[$item['PARAMS']['ID']] .= <<<HTML
-					<div class="header__submenu_tab-content tabcontent $class" data-id="{$item['PARAMS']['ID']}">
-						<ul class="header__tab-content_list"> 
+			<div class="header__submenu_tab-contents">
+				<div class="header__submenu_tab-content $class" data-id="{$item['PARAMS']['ID']}">
+					<ul class="header__tab-content_list">
 			HTML;
 
-		//var_dump($item);
-		$isSection = false;
-		$rsElements = CIBlockElement::GetList(["SORT" => "ASC"], ['IBLOCK_ID' => 5, 'IBLOCK_SECTION_ID' => $item['PARAMS']['ID']], false, false, []);
-		while ($arElement = $rsElements->GetNext()) {
-			//var_dump($arElement);
-
-			if ($arElement) {
-				$isSection = true;
-			}
-		}
-		if ($isSection) {
-			if ($item['PARAMS']['ELEMENT_CNT'] > 0) {
+		if ($item['PARAMS']['ELEMENT_CNT'] > 0) {
+			$rsElements = CIBlockElement::GetList(["SORT" => "ASC"], ['IBLOCK_ID' => 5, 'IBLOCK_SECTION_ID' => $item['PARAMS']['ID']], false, false, []);
+			while ($arElement = $rsElements->GetNext()) {
 				$arr_child[$item['PARAMS']['ID']] .= <<<HTML
-						<li>
-							<div class="header__submenu_tab-content_item">
+					<a href="/services/{$arElement['CODE']}/" class="header__tab-content_title">{$arElement['NAME']}</a>
 				HTML;
-				$rsElements = CIBlockElement::GetList(["SORT" => "ASC"], ['IBLOCK_ID' => 5, 'IBLOCK_SECTION_ID' => $item['PARAMS']['ID']], false, false, []);
-				while ($arElement = $rsElements->GetNext()) {
-					$arr_child[$item['PARAMS']['ID']] .= <<<HTML
-					<a href="/services/{$arElement['CODE']}/">{$arElement['NAME']}</a>
-				HTML;
-				}
-				$arr_child[$item['PARAMS']['ID']] .= <<<HTML
-					</div>
-					<button class="header__submenu_more-btn">Еще</button>
-				</li>
-			HTML;
 			}
 		}
 		$sections = CIBlockSection::GetList(array("SORT" => "ASC"), ["SECTION_ID" => $item['PARAMS']['ID']], true, []);
@@ -82,10 +66,56 @@ if (CModule::IncludeModule("iblock")) {
 		$arr_child[$item['PARAMS']['ID']] .= <<<HTML
 					</ul> 	
 				</div>	
+			</div>		
 			HTML;
-	endforeach;
+	};
 }
+
+
+/*
 ?>
+
+
+
+
+
+
+
+
+					<div class="header__submenu_tab-content active" style="order: 2">
+						<ul class="header__tab-content_list">
+							<li>
+								<a href="#" class="header__tab-content_title">
+									Лечение наркомании
+									<span>от 5 000 руб</span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="header__tab-content_title">
+									тест Наркомания
+									<span>от 5 000 руб</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <li class="header__nav_item header__nav_service hide-item">
 	<button class="hide-item__link hide-item_service">
@@ -137,6 +167,129 @@ if (CModule::IncludeModule("iblock")) {
 					</div>
 				</div>
 			</div> -->
+		</div>
+	</div>
+</li>
+
+
+
+
+
+
+
+
+
+
+
+					<div class="header__submenu_tab-content active" style="order: 2">
+						<ul class="header__tab-content_list">
+							<li>
+								<a href="#" class="header__tab-content_title">
+									Лечение наркомании
+									<span>от 5 000 руб</span>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="header__tab-content_title">
+									тест Наркомания
+									<span>от 5 000 руб</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+
+*/ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<li class="header__nav_item header__nav_service hide-item">
+	<button class="hide-item__link hide-item_service">
+		<div class="header__nav_service-span">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<span>Услуги</span>
+	</button>
+	<!--  Меню 2-го уровня: service-submenu-1 (3 варианта) -->
+	<!--  Меню 3-го уровня: service-subsubmenu-1 (3 варианта) -->
+	<!-- service-submenu-1 -->
+	<div class="header__submenu service-submenu-1">
+		<div class="container service-submenu-1__row">
+			<div class="header__submenu_tabs">
+				<div class="header__submenu_tab-btns">
+
+					<?= implode($arr_tabs); ?>
+
+				</div>
+
+				<div class="header__submenu_tab-contents">
+
+					<?= implode($arr_child); ?>
+
+				</div>
+			</div>
+
+			<!-- <div class="header__submenu-wrapper">
+				<div class="header__submenu_stocks">
+					<div class="header__stock">
+						<div>
+							<p class="header__stock_date">Действует до 23.05.2024</p>
+							<p class="header__stock_title">Скидка на услуги до 15%</p>
+							<p class="header__stock_subtitle">
+								Наши доктора помогают избавиться пациентам от
+								хронических зависимостей. Повышаем качество жизни
+								клиентов, их родственников, родных и близких.
+							</p>
+						</div>
+						<a href="#" class="header__stock_link btn-arrow"><span>Подробнее</span></a>
+					</div>
+
+					<div class="header__stock">
+						<div>
+							<p class="header__stock_date">Действует до 23.05.2024</p>
+							<p class="header__stock_title">Скидка на услуги до 15%</p>
+							<p class="header__stock_subtitle">
+								Наши доктора помогают избавиться пациентам от
+								хронических зависимостей. Повышаем качество жизни
+								клиентов, их родственников, родных и близких.
+							</p>
+						</div>
+						<a href="#" class="header__stock_link btn-arrow"><span>Подробнее</span></a>
+					</div>
+
+					<div class="header__stock">
+						<div>
+							<p class="header__stock_date">Действует до 23.05.2024</p>
+							<p class="header__stock_title">Скидка на услуги до 15%</p>
+							<p class="header__stock_subtitle">
+								Наши доктора помогают избавиться пациентам от
+								хронических зависимостей. Повышаем качество жизни
+								клиентов, их родственников, родных и близких.
+							</p>
+						</div>
+						<a href="#" class="header__stock_link btn-arrow"><span>Подробнее</span></a>
+					</div>
+				</div>
+			</div> -->
+
 		</div>
 	</div>
 </li>
